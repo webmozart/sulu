12d7f97e02dd89a6750ac650b725ff80566c26f2
Daniel Rotter <daniel.rotter@gmail.com>
2015-07-23 09:32:37 +0200

    install jackrabbit only when tests are run with jackrabbit

--- bin/jackrabbit.sh
+++ bin/jackrabbit.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+if [ "$SULU_PHPCR" = "jackrabbit" ]; then
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    cd $DIR
 
-VERSION=2.10.1
+    VERSION=2.10.1
+    JAR=jackrabbit-standalone-$VERSION.jar
 
-JAR=jackrabbit-standalone-$VERSION.jar
+    # download jackrabbit jar from archive, as the dist only contains the latest
+    # stable versions
+    if [ ! -f "$DIR/$JAR" ]; then
+        wget http://archive.apache.org/dist/jackrabbit/$VERSION/$JAR
+    fi
 
-# download jackrabbit jar from archive, as the dist only contains the latest
-# stable versions
-if [ ! -f "$DIR/$JAR" ]; then
-    wget http://archive.apache.org/dist/jackrabbit/$VERSION/$JAR
+    java -jar $DIR/$JAR&
+
+    echo "Waiting until Jackrabbit is ready on port 8080"
+    while [[ -z `curl -s 'http://localhost:8080' ` ]]
+    do
+        echo -n "."
+        sleep 2s
+    done
+
+    echo "Jackrabbit is up"
+
+    cd $DIR/..
+    php bin/jackrabbit.php
 fi
-
-java -jar $DIR/$JAR&
-
-echo "Waiting until Jackrabbit is ready on port 8080"
-while [[ -z `curl -s 'http://localhost:8080' ` ]]
-do
-    echo -n "."
-    sleep 2s
-done
-
-echo "Jackrabbit is up"
